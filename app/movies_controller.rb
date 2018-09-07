1dd92e52b71beed3655392a6b20e148ad47c9e80
Nicky Liu <nickyliu91@gmail.com>
2018-09-07 06:12:09 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,9 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = attributes.each {|key, value| self.send(("#{key}="), value)}
+
+
+  Movie.new(attributes).save
 end
 
 def can_be_created_in_a_block(args = __)
